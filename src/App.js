f9331007d5c467a3212ea61d2840250c0dda0f81
silbjava <silvanomth@gmail.com>
2024-02-02 18:09:17 -0300

    alteraçoes do style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Banner from './componentes/Banner/Banner';
 
 function App() {
   return (
     <div className="App">
+      <Banner/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+        <p>
+          Bem vindo ao Reacts.
         </p>
         <a
           className="App-link"
---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,9 @@
+import './Banner.css'
+export default function Banner(){
+    return(
+        //JSX
+        <header >
+            <img src="/imagens/banner.png" alt="Banner da pagina do organo"/>
+        </header>
+    )    
+}
